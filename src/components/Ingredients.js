fa47eed31587be508da16f602bee72bb5063cb26
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-01-13 12:14:30 +0900

    feat: 디렉토리 구조와 파일 생성

---
+++ src/components/Ingredients.js
@@ -0,0 +1,49 @@
+const data = [{
+    "name": 'fried-salmon',
+    "ingredients": [{
+        "name": "salmon",
+        "amount": 500,
+        "measurement": 'g'
+      }, {
+        "name": "마늘",
+        "amount": 3,
+        "measurement": '쪽'
+      },
+      {
+        "name": "올리브오일",
+        "amount": 0.5,
+        "measurement": '컵'
+      },
+    ],
+    "steps": ["오븐을 360도로 예열한다", "유리 베이킹 그릇에 올리브오일을 두른다", "그릇에 마늘과 연어를 담는다.", "오븐에서 30분간 익힌다.", "15분간 식힌 후 내놓는다."]
+  },
+  {
+    "name": '생선 타코',
+    "ingredients": [{
+        "name": "흰살생선",
+        "amount": 500,
+        "measurement": 'g'
+      }, {
+        "name": "토마토",
+        "amount": 2,
+        "measurement": '개'
+      },
+      {
+        "name": "치즈",
+        "amount": 1,
+        "measurement": '컵'
+      },
+      {
+        "name": "올리브오일",
+        "amount": 0.5,
+        "measurement": '컵'
+      },
+      {
+        "name": "또띠야",
+        "amount": 3,
+        "measurement": '개'
+      },
+    ],
+    "steps": ["생선을 그릴에 익힌다", "또띠야 3장 위에 생선을 얹는다.", "또띠야에 얹은 생선 위에 나머지 재료를 얹는다"]
+  }
+]
